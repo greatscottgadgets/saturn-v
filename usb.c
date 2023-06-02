cfb5c7b65616d8a5b243c4448bd3b7c45d652995
mndza <diego.hdmp@gmail.com>
2023-06-02 20:54:13 +0200

    simpler USB handlers
    - Remove unused SRAM DFU alt setting
    - This makes all descriptors < 64 bytes, which lets us ditch multi-packet code.
    - Refactor usb_cb_get_descriptor for size reduction.

--- usb.c
+++ usb.c
@@ -11,9 +11,8 @@
 #include "usb.h"
 #include "samd/usb_samd.h"
 
-// Return a ptr to a descriptor, perhaps made with usb_string_to_descriptor()
-// that contains a unique serial number for this board.
-void* get_serial_number_string_descriptor();
+// Return a string that contains a unique serial number for this board.
+char* get_serial_number_string();
 
 USB_ENDPOINTS(1);
 
@@ -42,8 +41,6 @@ typedef struct ConfigDesc {
 	USB_ConfigurationDescriptor Config;
 	USB_InterfaceDescriptor dfu_intf_flash;
 	DFU_FunctionalDescriptor dfu_desc_flash;
-	USB_InterfaceDescriptor dfu_intf_ram;
-	DFU_FunctionalDescriptor dfu_desc_ram;
 } ConfigDesc;
 
 const ConfigDesc configuration_descriptor = {
@@ -66,28 +63,9 @@ const ConfigDesc configuration_descriptor = {
 		.bInterfaceClass = DFU_INTERFACE_CLASS,
 		.bInterfaceSubClass = DFU_INTERFACE_SUBCLASS,
 		.bInterfaceProtocol = DFU_INTERFACE_PROTOCOL,
-		.iInterface = 0x10
+		.iInterface = 0x02
 	},
 	.dfu_desc_flash = {
-		.bLength = sizeof(DFU_FunctionalDescriptor),
-		.bDescriptorType = DFU_DESCRIPTOR_TYPE,
-		.bmAttributes = DFU_ATTR_CAN_DOWNLOAD | DFU_ATTR_WILL_DETACH,
-		.wDetachTimeout = 0,
-		.wTransferSize = DFU_TRANSFER_SIZE,
-		.bcdDFUVersion = 0x0101,
-	},
-	.dfu_intf_ram = {
-		.bLength = sizeof(USB_InterfaceDescriptor),
-		.bDescriptorType = USB_DTYPE_Interface,
-		.bInterfaceNumber = 0,
-		.bAlternateSetting = 1,
-		.bNumEndpoints = 0,
-		.bInterfaceClass = DFU_INTERFACE_CLASS,
-		.bInterfaceSubClass = DFU_INTERFACE_SUBCLASS,
-		.bInterfaceProtocol = DFU_INTERFACE_PROTOCOL,
-		.iInterface = 0x11
-	},
-	.dfu_desc_ram = {
 		.bLength = sizeof(DFU_FunctionalDescriptor),
 		.bDescriptorType = DFU_DESCRIPTOR_TYPE,
 		.bmAttributes = DFU_ATTR_CAN_DOWNLOAD | DFU_ATTR_WILL_DETACH,
@@ -101,12 +79,6 @@ const USB_StringDescriptor language_string = {
 	.bLength = USB_STRING_LEN(1),
 	.bDescriptorType = USB_DTYPE_String,
 	.bString = {USB_LANGUAGE_EN_US},
-};
-
-const USB_StringDescriptor msft_os = {
-	.bLength = 18,
-	.bDescriptorType = USB_DTYPE_String,
-	.bString = {'M','S','F','T','1','0','0',0xee},
 };
 
 const USB_MicrosoftCompatibleDescriptor msft_compatible = {
@@ -126,6 +98,19 @@ const USB_MicrosoftCompatibleDescriptor msft_compatible = {
 	}
 };
 
+
+void* str_to_descriptor(char* str, uint8_t len) {
+	USB_StringDescriptor* desc = (((USB_StringDescriptor*)ep0_buf_in));
+	desc->bLength = USB_STRING_LEN(len);
+	desc->bDescriptorType = USB_DTYPE_String;
+	for (int i=0; i<len; i++) {
+		desc->bString[i] = str[i];
+	}
+	return desc;
+}
+
+#define STRLEN(x) ((sizeof(x)/sizeof(x[0])) - 1)
+
 uint16_t usb_cb_get_descriptor(uint8_t type, uint8_t index, const uint8_t** ptr) {
 	const void* address = NULL;
 	uint16_t size    = 0;
@@ -140,36 +125,38 @@ uint16_t usb_cb_get_descriptor(uint8_t type, uint8_t index, const uint8_t** ptr)
 			size    = sizeof(ConfigDesc);
 			break;
 		case USB_DTYPE_String:
+			char *string = NULL;
+			uint8_t string_len;
 			switch (index) {
 				case 0x00:
 					address = &language_string;
-					break;
+					size = language_string.bLength;
+					goto _exit;
 				case 0x01:
-					address = usb_string_to_descriptor(USB_MANUFACTURER_STR);
+					string = USB_MANUFACTURER_STR;
+					string_len = STRLEN(USB_MANUFACTURER_STR);
 					break;
 				case 0x02:
-					address = usb_string_to_descriptor(USB_PRODUCT_STR);
+					string = USB_PRODUCT_STR;
+					string_len = STRLEN(USB_PRODUCT_STR);
 					break;
 				case 0x03:
-					address = get_serial_number_string_descriptor();
-					break;
-				case 0x10:
-					address = usb_string_to_descriptor("Flash");
-					break;
-				case 0x11:
-					address = usb_string_to_descriptor("SRAM");
-					break;
-				case 0xf0:
-					address = usb_string_to_descriptor("");
+					string = get_serial_number_string();
+					string_len = 26;
 					break;
 				case 0xee:
-					address = &msft_os;
+					string = "MSFT100\xee";
+					string_len = 8;
 					break;
+				default:
+					goto _exit;
 			}
+			address = str_to_descriptor(string, string_len);
 			size = (((USB_StringDescriptor*)address))->bLength;
 			break;
 	}
 
+	_exit:
 	*ptr = address;
 	return size;
 }
@@ -230,32 +217,19 @@ bool usb_cb_set_interface(uint16_t interface, uint16_t altsetting) {
 	return false;
 }
 
-// Return a string descriptor containing a unique serial number.
-//
-void *get_serial_number_string_descriptor()
+/**
+ * Returns a string that describes this device's unique ID.
+ */
+char *get_serial_number_string(void)
 {
-#if 0
-	char buf[27];
-
-	const unsigned char* id = (unsigned char*) 0x0080A00C;
-	for (int i=0; i<26; i++) {
-		unsigned idx = (i*5)/8;
-		unsigned pos = (i*5)%8;
-		unsigned val = ((id[idx] >> pos) | (id[idx+1] << (8-pos))) & ((1<<5)-1);
-		buf[i] = "0123456789ABCDFGHJKLMNPQRSTVWXYZ"[val];
-	}
-	buf[26] = 0;
-#endif
-
-	//
-	// Read and save the device serial number as normal Base32.
-	//
-
 	// Documented in section 9.3.3 of D21 datasheet, page 32 (rev G), but no header file,
 	// these are not contiguous addresses.
 	const uint32_t	*ser[4] = {
-		 	(uint32_t *)0x0080A00C,
-			(uint32_t *)0x0080A040, (uint32_t *)0x0080A044, (uint32_t *)0x0080A048 };
+		(uint32_t *)0x0080A00C,
+		(uint32_t *)0x0080A040,
+		(uint32_t *)0x0080A044,
+		(uint32_t *)0x0080A048
+	};
 
 	uint32_t copy[5];
 	copy[0] = *ser[0];
@@ -283,6 +257,6 @@ void *get_serial_number_string_descriptor()
 		buf[count] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"[index];
 	}
 
-	return usb_string_to_descriptor(buf);
-}
+	return buf;
 
+}
